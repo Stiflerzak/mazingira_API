8d69fb8ef4409e26eee35269a3738a98edc12442
Stiflerzak <hilarymwendia97@gmail.com>
2023-04-29 18:41:15 +0300

    added cors

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module MazingiraApi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use config.session_store, config.session_options
+    config.action_dispatch.cookies_same_site_protection = :strict
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
